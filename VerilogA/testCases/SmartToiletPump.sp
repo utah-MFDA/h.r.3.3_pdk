5f24a8415961fabd1c19bfb7e72f5b03492f18f6
snielsen221b <sophianielsen221b@gmail.com>
2022-02-09 15:00:00 -0700

    update dimension calcs, update smart toilet chip flow  test

---
+++ VerilogA/testCases/SmartToiletPump.sp
@@ -0,0 +1,25 @@
+*serpentine channel test
+
+.hdl ./../../../SerpentineChannel.va
+.hdl ./../../../RectangularChannel.va
+.hdl ./../../../TJunction.va
+.hdl ./../../../PressurePump.va
+.options post=1
+
+X1 A 0 cA cD1 PressurePump pressure=34450 chem_concentration=0
+X2 B 0 cB cD2 PressurePump pressure=34450 chem_concentration=0
+X3 C 0 cC cE2 PressurePump pressure=34450 chem_concentration=0
+
+X4 A D1 cA cD1 SerpentineChannel
+X5 B D2 cB cD2 RectangularChannel L=0.00071
+X6 C E2 cC cE2 SerpentineChannel FOOTPRINT_WIDTH = 0.01362
+
+X7 D1 D2 Di Dout cD1 cD2 cDout TJunction L=0.0001
+
+X8 Dout E1 cDout cE1 SerpentineChannel
+
+X9 E1 E2 Ei 0 cE1 cE2 cEout TJunction L=0.0001
+
+.tran 0.01ms 8ms
+
+.end
--- dimension_calcs.py
+++ dimension_calcs.py
@@ -13,9 +13,7 @@ def straight_channel_resistence(L, nu, width_pixels, hieght_layers):
     print(R_hyd)
     return R_hyd
 
-def serpentine_channel_resistence():
-    footprint_width = 0.001064 # preset for  5 BENDS
-    nu = 0.0010016 # water at 20 deg C, [Pa s], [N s/m2]
+def serpentine_channel_resistence(footprint_width, nu):
 
     microns_per_pixel = 7.6
     microns_per_layer = 10
@@ -27,18 +25,18 @@ def serpentine_channel_resistence():
     width_pixels = 14;
     hieght_layers = 8;
 
-    channel_spacing = channel_spacing_pixels*microns_per_pixel*meters_per_micron;
+    channel_spacing = channel_spacing_pixels*microns_per_pixel*meters_per_micron
 
-    W = width_pixels*microns_per_pixel*meters_per_micron;
-    H = hieght_layers*microns_per_layer*meters_per_micron;
+    W = width_pixels*microns_per_pixel*meters_per_micron
+    H = hieght_layers*microns_per_layer*meters_per_micron
 
-    bends = footprint_width/(W+channel_spacing);
+    bends = footprint_width/(W+channel_spacing)
 
-    effective_footprint_width = bends*(W+channel_spacing);
+    effective_footprint_width = bends*(W+channel_spacing)
 
-    L = bends*effective_footprint_width + effective_footprint_width;
+    L = bends*effective_footprint_width + effective_footprint_width
 
-    R_hyd = (12*nu*L)/(1 - 0.63*(H/W)) * 1/(H*H*H/W);
+    R_hyd = (12*nu*L)/(1 - 0.63*(H/W)) * 1/(H*H*H/W)
 
     print(R_hyd)
 
@@ -54,42 +52,37 @@ def length_from_resistence(R_hyd, nu, width_pixels, hieght_layers):
 
     L = R_hyd * (math.pow(H,3)/W) * (1 - 0.63*(H/W))/(12*nu)
 
-    print(L)
     return(L)
 
-def footprint_from_resistence():
-    R_hyd = 26893665.72900001
-
-    nu = 0.0010016 # water at 20 deg C, [Pa s], [N s/m2]
+def footprint_from_resistence(R_hyd, nu, width_pixels, hieght_layers):
 
     microns_per_pixel = 7.6
     microns_per_layer = 10
     meters_per_micron = 0.000001
 
-    channel_spacing_pixels = 14
-
-    # for cross section 1
-    width_pixels = 14;
-    hieght_layers = 8;
-
-    channel_spacing = channel_spacing_pixels*microns_per_pixel*meters_per_micron;
+    L = length_from_resistence(R_hyd, nu, width_pixels, hieght_layers)
 
     W = width_pixels*microns_per_pixel*meters_per_micron;
     H = hieght_layers*microns_per_layer*meters_per_micron;
 
-    L = R_hyd * (math.pow(H,3)/W) * (1 - 0.63*(H/W))/(12*nu)
+    channel_spacing_pixels = 14
+    channel_spacing = channel_spacing_pixels*microns_per_pixel*meters_per_micron
 
-    L = math.pow(footprint_width,2)/(W+channel_spacing) + footprint_width;
+    footprint_width = 0.5*(math.sqrt(channel_spacing+W)*math.sqrt(channel_spacing+4*L+W) - channel_spacing - W)
+    bends =  footprint_width/(W + channel_spacing)
 
-    print(footprint_width)
+    bends = round(bends)
+
+    effective_footprint_width = bends*(W+channel_spacing)
+
+    print(effective_footprint_width)
+    print(bends)
+    return(effective_footprint_width)
 
 def flow_rate(R_hyd, del_P):
     flow_rate = del_P/R_hyd
     print(flow_rate)
     return flow_rate
 
-#footprint_from_resistence()
-#length_from_resistence(3366.29, 0.0010016, 14, 8)
-#straight_channel_resistence(0.0007093338693968819, 0.001, 14, 8)
-flow_rate(30770.4, 34450)
-#straight_channel_resistence(0.0001, 0.001, 14, 8)
+serpentine_channel_resistence(0.013619199999999998, 0.0010016)
+footprint_from_resistence(4165780, 0.0010016, 14, 8)
