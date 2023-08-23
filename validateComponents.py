5fbf2483aee10370a2deba21c05c65c8fd5b887c
Brady Goenner <brady.goenner@utah.edu>
2023-08-22 18:37:56 -0600

    lef and scad

---
+++ validateComponents.py
@@ -0,0 +1,20 @@
+
+
+def get_data_base():
+    pass
+
+def check_lef_file():
+    pass
+
+def check_scad_file():
+    pass
+
+def check_va_file():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
