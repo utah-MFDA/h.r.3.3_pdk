0f59e76623380d3f03826278835a241a07636882
Brady Goenner <brady.goenner@utah.edu>
2023-08-24 12:46:06 -0600

    new circuit tests

---
+++ VerilogA/xyce_testing/ElibraryTesting/pCellTesting/runtest.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+lib=./../../../Elibrary/xyce_lib/Elibrary/libElibrary.so
+
+#echo $hardware
+
+Xyce -plugin $lib channelTest.cir
--- VerilogA/xyce_testing/pumpTesting/runtest.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-hardware=./../../Elibrary/xyce_lib/hardware/libXyce_Plugin_EPressurePump.so
-
-#echo $hardware
-
-Xyce -plugin $hardware pressurePumpTest.cir
