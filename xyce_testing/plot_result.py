7ed48aa349d2084a30a23764bb359c394bcccb6f
Brady Goenener <bradygoenner@gmail.com>
2024-03-08 09:00:07 -0700

    valve and membrane testing and validation

--- xyce_testing/plot_result.py
+++ xyce_testing/plot_result.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import re
+import os, shutil
 
 def load_prn(results_file, plot_nodes=None, plot_type=None):
 
@@ -13,6 +14,24 @@ def load_prn(results_file, plot_nodes=None, plot_type=None):
                         #delim_whitespace=True, 
                         sep='\s+')
 
+def sum_cur(time, i, set_time_inter=None):
+    i_sum = 0
+    t_sl = [0,0]
+    if set_time_inter is not None:
+        if isinstance(set_time_inter, list) and len(set_time_inter) == 2:
+            for ind, ti in enumerate(set_time_inter):
+                for t_ind, t in enumerate(time):
+                    if t >= ti:
+                        t_sl[ind] = t_ind
+                        break
+            time = time.iloc[t_sl[0]:t_sl[1]].reset_index(drop=True)
+    
+    for ind, t in enumerate(time):
+        if ind == 0:
+            continue
+        else:
+            i_sum += (i[ind] - i[ind-1])*(time[ind-1] - time[ind])
+    return i_sum
 
 # plot_nodes is a list of strings
 def plot_prn(results_file, plot_nodes=None, plot_type=None):
@@ -67,7 +86,61 @@ def plot_prn(results_file, plot_nodes=None, plot_type=None):
     ax.legend()
     print("plot: "+results_file.split('/')[-1])
     plt.show()
+    
+def run_xyce(lib, cir):
+    if not os.path.isfile(lib):
+        raise ValueError(lib + " library file does not exist") 
 
+    if not os.path.isfile(cir):
+        raise ValueError(cir + " not able to find ciruit file") 
+
+    cir_root, fname = os.path.split(cir)
+    
+    subprocess.run(['sh', './../run_test.sh', lib, cir, cir_root])
+
+component_loc = './../Components/'
+xyce_build    = '/usr/local/bin/buildxyceplugin -o'.split(' ')
+
+def build_and_run(cir, modules):
+    cir_root, fname = os.path.split(cir)
+    src_path = cir_root+'/.'+fname+'_src'
+    os.mkdir(src_path)
+
+    lib_file = component_loc+'/veriloga_objects/MFXyce'
+    shutil.copy(lib_file, src_path+'/ComponentLib')
+    
+    for m in modules:
+        if len(m) == 2:
+            mfile = component_loc+'/'+m[0]+'/'+m[1]+'/'+m[1]+'.va'
+        elif len(m) == 3:
+            mfile = component_loc+'/'+m[0]+'/'+m[1]+'/'+m[2]+'.va'
+        shutil.copyfile(mfile, src_path)
+    subprosses.run(xyce_build)
+
+    lib = src_path+'/'
+                             
+    run_and_plot(lib, cir, plot_type=None, plot_nodes=None)
+
+def run_and_plot(lib, cir, plot_type=None, plot_nodes=None, cir_path=None):
+
+    cir_root, fname = os.path.split(cir)
+    cir_a = cir_root+'/'+cir
+    prn   = cir_root+'/results/'+cir+'.prn'
+    
+    run_xyce(lib, cir_a)
+    
+    # only the definition above needs to be changed
+    run_xyce(lib, cir_a)
+    if plot_type is not None:
+        for ind, ty in enumerate(plot_type):
+            if plot_node is not None:
+                n=plot_nodes[ind]
+            else:
+                n=None
+            plot_r(prn, plot_type=ty, plot_nodes=n)
+    else:
+        plot_r(prn, plot_type=None, plot_nodes=plot_nodes)
+        
 
 if __name__ == "__main__":
 
